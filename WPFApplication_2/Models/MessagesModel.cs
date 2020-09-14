7a1faa01f613d07b8fa8499f3484ce2cc1ef1550
johanssonida1996@gmail.com <johanssonida1996@gmail.com>
2020-09-14 14:34:03 +0200

    Add project files.

---
+++ WPFApplication_2/Models/MessagesModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFApplication_2.Models
+{
+    class MessagesModel
+    {
+    }
+}
---
+++ WPFApplication_2/Views/CalendarViewModel.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApplication_2.Views
+{
+    /// <summary>
+    /// Interaction logic for CalendarViewModel.xaml
+    /// </summary>
+    public partial class CalendarViewModel : UserControl
+    {
+        public CalendarViewModel()
+        {
+            InitializeComponent();
+        }
+    }
+}
