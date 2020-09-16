10d2277c1070a3c7b308e4c304d967868529e847
johanssonida1996@gmail.com <johanssonida1996@gmail.com>
2020-09-16 11:58:13 +0200

    Change1.

---
+++ WPFApplication_2/Controls/MessagesControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApplication_2.Controls
+{
+    /// <summary>
+    /// Interaction logic for MessagesControl.xaml
+    /// </summary>
+    public partial class MessagesControl : UserControl
+    {
+        public MessagesControl()
+        {
+            InitializeComponent();
+        }
+
+        public ImageSource MessagesImage
+        {
+            get { return messagesImage.Source; }
+            set { messagesImage.Source = value; }
+        }
+
+        public string MessagesName
+        {
+            get { return messagesName.Text; }
+            set { messagesName.Text = value; }
+        }
+
+        public string MessagesInfo
+        {
+            get { return messagesInfo.Text; }
+            set { messagesInfo.Text = value; }
+        }
+    }
+}
---
+++ WPFApplication_2/Controls/TestControlPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApplication_2.Controls
+{
+    /// <summary>
+    /// Interaction logic for TestControlPage.xaml
+    /// </summary>
+    public partial class TestControlPage : UserControl
+    {
+        public TestControlPage()
+        {
+            InitializeComponent();
+        }
+        public ImageSource ContactImage
+        {
+            get { return contactImage.Source; }
+            set { contactImage.Source = value; }
+        }
+
+        public string ContactName
+        {
+            get { return contactName.Text; }
+            set { contactName.Text = value; }
+        }
+
+        public string ContactRole
+        {
+            get { return contactRole.Text; }
+            set { contactRole.Text = value; }
+        }
+    }
+}
