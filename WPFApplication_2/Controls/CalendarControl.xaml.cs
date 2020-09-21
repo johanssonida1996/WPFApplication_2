1918925995367af462953799ccfdfa0774d572a5
johanssonida1996@gmail.com <johanssonida1996@gmail.com>
2020-09-21 14:55:50 +0200

    2020-09-21

---
+++ WPFApplication_2/Controls/CalendarControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApplication_2.Controls
+{
+    /// <summary>
+    /// Interaction logic for CalendarControl.xaml
+    /// </summary>
+    public partial class CalendarControl : UserControl
+    {
+        public CalendarControl()
+        {
+            InitializeComponent();
+        }
+        public ImageSource CalendarImage
+        {
+            get { return calendarImage.Source; }
+            set { calendarImage.Source = value; }
+        }
+
+        public string CalendrName
+        {
+            get { return calendarName.Text; }
+            set { calendarName.Text = value; }
+        }
+
+        public string CalendarInfo
+        {
+            get { return calendarInfo.Text; }
+            set { calendarInfo.Text = value; }
+        }
+    }
+}
---
+++ WPFApplication_2/Controls/SettingControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApplication_2.Controls
+{
+    /// <summary>
+    /// Interaction logic for SettingControl.xaml
+    /// </summary>
+    public partial class SettingControl : UserControl
+    {
+        public SettingControl()
+        {
+            InitializeComponent();
+        }
+        public ImageSource SettingImage
+        {
+            get { return settingImage.Source; }
+            set { settingImage.Source = value; }
+        }
+
+        public string SettingName
+        {
+            get { return settingName.Text; }
+            set { settingName.Text = value; }
+        }
+
+        public string SettingInfo
+        {
+            get { return settingInfo.Text; }
+            set { settingInfo.Text = value; }
+        }
+    }
+}
